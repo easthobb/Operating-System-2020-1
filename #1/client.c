893bcf4a44c4c1de2f5676690651e57f9f7ac330
hobbes1520@gmail.com <receiver@kakao.com>
2020-06-14 01:37:00 +0900

    function fully implemented

--- #1/client.c
+++ #1/client.c
@@ -6,13 +6,13 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 55000
+#define PORT 5500
 int main(){
 
 int clientSocket;
 struct sockaddr_in server_address;
-char toServer[]="123456789";
-char fromServer[100];
+//char toServer[]="123456789";
+//char fromServer[100];
 int str_len;
 
 
@@ -27,15 +27,41 @@ server_address.sin_addr.s_addr=inet_addr("127.0.0.1");
 server_address.sin_port=htons(PORT);
 
 /*서버에 접속*/
-connect(clientSocket, (struct sockaddr*)&server_address, sizeof(server_address));
-printf("Connect Server!!\n");
+if(connect(clientSocket, (struct sockaddr*)&server_address, sizeof(server_address))==-1)
+{
+    printf("sorry, connection failed!");
+    return 0;
+}
+printf("Connect to Server is success!!\n");
 
+
+while(1){
 /*toServer 에 저장된 메시지를 서버로 전송*/ 
+//char toServer[100];
+//char fromServer[100];
+char toServer[100];
+char fromServer[100];
+
+printf("Enter Server Message want to send (Bye will be closed connection): ");
+scanf("%[^\n]s",toServer);
+fflush(stdin);
+toServer[99]='\0';
+
+if(strcmp(toServer,"Bye")==0)
+{
+    printf("connection is closing. thx you!\n");
+    write(clientSocket,toServer,sizeof(toServer));
+    break;
+}
 write(clientSocket,toServer,sizeof(toServer));
-printf("To Server Message: %s\n",toServer);
 
 /*서버에서 보내온 메시지를 fromServer 에 저장*/ 
 read(clientSocket, fromServer, sizeof(fromServer)); 
 printf("From Server Message: %s\n",fromServer);
-close(clientSocket); return 0;
+
+
+}
+
+close(clientSocket); 
+return 0;
 }
--- #1/server.c
+++ #1/server.c
@@ -5,7 +5,7 @@
 #include <string.h> 
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 55000
+#define PORT 5500
 
 int main(){
 
@@ -13,10 +13,10 @@ int s_socket_fd;
 int c_socket_fd;
 struct sockaddr_in server_address, client_address;
 socklen_t client_address_size;
-char Msg2Client[]="Hello Client!!\n"; 
-char Msg2Server[100];
-char fromClient[100];
-char toClient[100];
+//char Msg2Client[]="Hello Client!!\n"; 
+//char Msg2Server[100];
+//char fromClient[100];
+//char toClient[100];
 
 /*클라이언트의 접속을 기다리는 서버소켓 객체 생성*/ 
 s_socket_fd = socket(PF_INET, SOCK_STREAM, 0);
@@ -31,24 +31,56 @@ server_address.sin_addr.s_addr=htonl(INADDR_ANY);
 server_address.sin_port=htons(PORT);
 bind(s_socket_fd, (struct sockaddr*) &server_address, sizeof(server_address));
 
+
 /*클라이언트의 접속을 기다리는 대기 모드로 전환*/ 
 listen(s_socket_fd, 5);
 printf("Wait Client...\n");
-client_address_size = sizeof(client_address);
 
-/*클라이언트가 서버에 접속하면 접속을 수락함*/
-c_socket_fd = accept(s_socket_fd, (struct sockaddr*)&client_address,&client_address_size); 
+client_address_size = sizeof(client_address);
+c_socket_fd = accept(s_socket_fd, (struct sockaddr*)&client_address,&client_address_size);
+if(c_socket_fd < 0)
+{
+    printf("Server: accept failed.\n");
+    exit(0);
+}
+
+/*클라이언트가 서버에 접속하면 접속을 수락함*/ 
 printf("Client Connect!!!\n");
 
+while(1){
+    //
 /*클라이언트가 전달한 메시지를 읽어 들여서 fromClient에 대입*/ 
-read(c_socket_fd, Msg2Server ,sizeof(fromClient));
-printf("From Client Message: %s\n",Msg2Server);
+char toClient[100];
+char fromClient[100];
+
+read(c_socket_fd, fromClient ,sizeof(fromClient));
+
+printf("From Client Message: %s\n",fromClient);
+if(strcmp("Bye",fromClient)==0)
+{   
+    printf("Connection is closed.\n");
+    break;
+}
+printf("\nMessage To Client:");
+
+scanf("%[^\n]s",toClient);
+fflush(stdin);
 
 /* 이 자리에서 Client로부터 요청받은 서비스를 수행 */
-/* Msg2Client 의 내용을 클라이언트로 전송 */ 
-write(c_socket_fd, Msg2Client, sizeof(toClient)); 
-printf("Message To Client: %s\n",Msg2Client);
-/*클라이언트와의 접속을 종료함*/ 
+/* Msg2Client 의 내용을 클라이언트로 전송 */
+//if(toClient!=NULL){ 
+write(c_socket_fd, toClient, sizeof(toClient));
+//}
+
+//printf("Message To Client: %s\n",Msg2Client);
+/*클라이언트와의 접속을 종료함*/
+if(c_socket_fd < 0)
+{
+    printf("Server: connection closed.\n");
+    exit(0);
+}
+} 
+
 close(c_socket_fd);
 return 0;
 }
