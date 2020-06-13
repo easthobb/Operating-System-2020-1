433d39f43eb7351447663b7483022b6f5fd02fa6
hobbes1520@gmail.com <receiver@kakao.com>
2020-06-14 05:15:52 +0900

    #2 implement still not debuged

--- #2/client.c
+++ #2/client.c
@@ -5,63 +5,219 @@
 #include <string.h> 
 #include <sys/socket.h>
 #include <unistd.h>
+#include <unistd.h>
 
-#define PORT 5500
+
+#define PORT 10829
 int main(){
 
 int clientSocket;
 struct sockaddr_in server_address;
-//char toServer[]="123456789";
-//char fromServer[100];
 int str_len;
 
+/* 본 코드는 fork()를 통해 총 4개의 프로세스를 생성하고 실행하며 구조는 다음과 같다.*/
+/* P - (P-C1) -(P-C1-C3)*/
+/* |                    */
+/* (P-C2)               */
 
-/*서버에 접속해서 데이터를 입출력 하는 클라이언트 소켓생성*/ 
-clientSocket=socket(PF_INET, SOCK_STREAM, 0);
-printf("Create Client Socket!!\n");
-/*서버의 주소 정보가 저장될 server_adress 를 0으로 초기화*/ 
-memset(&server_address, 0,sizeof(server_address));
-/*server_address 에 서버의 주소 정보 대입*/ 
-server_address.sin_family=AF_INET;
-server_address.sin_addr.s_addr=inet_addr("127.0.0.1"); 
-server_address.sin_port=htons(PORT);
-
-/*서버에 접속*/
-if(connect(clientSocket, (struct sockaddr*)&server_address, sizeof(server_address))==-1)
-{
-    printf("sorry, connection failed!");
-    return 0;
-}
-printf("Connect to Server is success!!\n");
-
-
-while(1){
-/*toServer 에 저장된 메시지를 서버로 전송*/ 
-//char toServer[100];
-//char fromServer[100];
-char toServer[100];
-char fromServer[100];
-
-printf("Enter Server Message want to send (Bye will be closed connection): ");
-scanf("%[^\n]s",toServer);
-fflush(stdin);
-toServer[99]='\0';
-
-if(strcmp(toServer,"Bye")==0)
-{
-    printf("connection is closing. thx you!\n");
-    write(clientSocket,toServer,sizeof(toServer));
-    break;
-}
-write(clientSocket,toServer,sizeof(toServer));
-
-/*서버에서 보내온 메시지를 fromServer 에 저장*/ 
-read(clientSocket, fromServer, sizeof(fromServer)); 
-printf("From Server Message: %s\n",fromServer);
-
-
+pid_t pid; // PID 선언
+pid = fork();
+if(pid == -1)
+{ //-1 이면 fork생성 에러
+    printf("can't fork, erro\n");
+	exit(0);
 }
 
-close(clientSocket); 
-return 0;
+if(pid == 0){ // P-C1이 실행하는 부분
+    
+    pid=fork();
+    
+    if(pid>0){ //P-C1가 실행하는 부분
+
+    /*PROCESS 별 반복 부분-함수화 요망*/
+    int PID_NUM = getpid(); // PID번호를 저장하는 변수
+
+    printf("I AM P-C1 PROCESS MY PID IS :%d\n",PID_NUM);
+    /*서버에 접속해서 데이터를 입출력 하는 클라이언트 소켓생성*/ 
+    clientSocket=socket(PF_INET, SOCK_STREAM, 0);
+    /*서버의 주소 정보가 저장될 server_adress 를 0으로 초기화*/
+    memset(&server_address, 0,sizeof(server_address));
+    
+    /*server_address 에 서버의 주소 정보 대입*/ 
+    server_address.sin_family=AF_INET;//IPv4 채택
+    server_address.sin_addr.s_addr=inet_addr("127.0.0.1"); //IP 주소입력(localhost)
+    server_address.sin_port=htons(PORT);//포트번호 입력
+
+    //입력된 정보에 해당하는 서버에 접속 시도 실패시 프로세스 종료
+    if(connect(clientSocket, (struct sockaddr*)&server_address, sizeof(server_address))==-1)
+    {
+        printf("sorry, connection failed! exit this process:%d\n",PID_NUM);
+        return 0;
+    }
+    else{ //연결에 성공했을 경우
+        for(int i = 0 ; i<5 ; i++){ // 메시지에 대해 5번 수행
+            char * toServer = malloc(sizeof(char)*100);
+            char temp[] = "Hello, Server this is message No." ;//서버에 보낼 문자열
+            char fromServer[100];
+            strcpy(toServer,temp); //임시변수에 들은 문자열 복사
+            strcat(toServer,(char*)i);//서버에 보낼 문자열에 sequence adding
+            strcat(toServer,"from"); //from adding
+            strcat(toServer,(char*)PID_NUM);//PID_NUM adding
+
+            /*toServer 문자열을 server로 쏩니다*/
+            printf("Client[PID:%d] now sending to Server\n",PID_NUM);
+            write(clientSocket,toServer,sizeof(toServer));
+            read(clientSocket, fromServer, sizeof(fromServer));
+            printf("Client[PID:%d] got message to Server%s\n",PID_NUM,fromServer);
+            free(toServer);  
+
+        }
+
+    }
+
+    sleep(3);  
+
+    }
+    else{//P-C1-C3가 실행하는 부분 
+ 
+    /*PROCESS 별 반복 부분-함수화 요망*/
+    int PID_NUM = getpid(); // PID번호를 저장하는 변수
+
+    printf("I AM P-C1-C3 PROCESS MY PID IS :%d\n",PID_NUM);
+    /*서버에 접속해서 데이터를 입출력 하는 클라이언트 소켓생성*/ 
+    clientSocket=socket(PF_INET, SOCK_STREAM, 0);
+    /*서버의 주소 정보가 저장될 server_adress 를 0으로 초기화*/
+    memset(&server_address, 0,sizeof(server_address));
+    
+    /*server_address 에 서버의 주소 정보 대입*/ 
+    server_address.sin_family=AF_INET;//IPv4 채택
+    server_address.sin_addr.s_addr=inet_addr("127.0.0.1"); //IP 주소입력(localhost)
+    server_address.sin_port=htons(PORT);//포트번호 입력
+
+    //입력된 정보에 해당하는 서버에 접속 시도 실패시 프로세스 종료
+    if(connect(clientSocket, (struct sockaddr*)&server_address, sizeof(server_address))==-1)
+    {
+        printf("sorry, connection failed! exit this process:%d\n",PID_NUM);
+        return 0;
+    }
+    else{ //연결에 성공했을 경우
+        for(int i = 0 ; i<5 ; i++){ // 메시지에 대해 5번 수행
+            char * toServer = malloc(sizeof(char)*100);
+            char temp[] = "Hello, Server this is message No." ;//서버에 보낼 문자열
+            char fromServer[100];
+            strcpy(toServer,temp); //임시변수에 들은 문자열 복사
+            strcat(toServer,(char*)i);//서버에 보낼 문자열에 sequence adding
+            strcat(toServer,"from"); //from adding
+            strcat(toServer,(char*)PID_NUM);//PID_NUM adding
+
+            /*toServer 문자열을 server로 쏩니다*/
+            printf("Client[PID:%d] now sending to Server\n",PID_NUM);
+            write(clientSocket,toServer,sizeof(toServer));
+            read(clientSocket, fromServer, sizeof(fromServer));
+            printf("Client[PID:%d] got message to Server%s\n",PID_NUM,fromServer); 
+            free(toServer); 
+
+        }
+
+    }
+    sleep(1);  
+    }
+
+}
+else{ // P1이 실행하는 부분
+    
+    pid=fork();
+    
+    if(pid==0){ //P-C2 가 실행하는 부분
+
+    /*PROCESS 별 반복 부분-함수화 요망*/
+    int PID_NUM = getpid(); // PID번호를 저장하는 변수
+
+    printf("I AM P-C2 PROCESS MY PID IS :%d\n",PID_NUM);
+    /*서버에 접속해서 데이터를 입출력 하는 클라이언트 소켓생성*/ 
+    clientSocket=socket(PF_INET, SOCK_STREAM, 0);
+    /*서버의 주소 정보가 저장될 server_adress 를 0으로 초기화*/
+    memset(&server_address, 0,sizeof(server_address));
+    
+    /*server_address 에 서버의 주소 정보 대입*/ 
+    server_address.sin_family=AF_INET;//IPv4 채택
+    server_address.sin_addr.s_addr=inet_addr("127.0.0.1"); //IP 주소입력(localhost)
+    server_address.sin_port=htons(PORT);//포트번호 입력
+
+    //입력된 정보에 해당하는 서버에 접속 시도 실패시 프로세스 종료
+    if(connect(clientSocket, (struct sockaddr*)&server_address, sizeof(server_address))==-1)
+    {
+        printf("sorry, connection failed! exit this process:%d\n",PID_NUM);
+        return 0;
+    }
+    else{ //연결에 성공했을 경우
+        for(int i = 0 ; i<5 ; i++){ // 메시지에 대해 5번 수행
+            char * toServer = malloc(sizeof(char)*100);
+            char temp[] = "Hello, Server this is message No." ;//서버에 보낼 문자열
+            char fromServer[100];
+            strcpy(toServer,temp); //임시변수에 들은 문자열 복사
+            strcat(toServer,(char*)i);//서버에 보낼 문자열에 sequence adding
+            strcat(toServer,"from"); //from adding
+            strcat(toServer,(char*)PID_NUM);//PID_NUM adding
+
+            /*toServer 문자열을 server로 쏩니다*/
+            printf("Client[PID:%d] now sending to Server\n",PID_NUM);
+            write(clientSocket,toServer,sizeof(toServer));
+            read(clientSocket, fromServer, sizeof(fromServer));
+            printf("Client[PID:%d] got message to Server%s\n",PID_NUM,fromServer);
+            free(toServer);  
+
+        }
+
+    }
+
+    sleep(3);  
+
+    }
+    else{// 찐 P1이 실행하는 부분
+    
+    /*PROCESS 별 반복 부분-함수화 요망*/
+    int PID_NUM = getpid(); // PID번호를 저장하는 변수
+
+    printf("I AM P1 PROCESS MY PID IS :%d\n",PID_NUM);
+    /*서버에 접속해서 데이터를 입출력 하는 클라이언트 소켓생성*/ 
+    clientSocket=socket(PF_INET, SOCK_STREAM, 0);
+    /*서버의 주소 정보가 저장될 server_adress 를 0으로 초기화*/
+    memset(&server_address, 0,sizeof(server_address));
+    
+    /*server_address 에 서버의 주소 정보 대입*/ 
+    server_address.sin_family=AF_INET;//IPv4 채택
+    server_address.sin_addr.s_addr=inet_addr("127.0.0.1"); //IP 주소입력(localhost)
+    server_address.sin_port=htons(PORT);//포트번호 입력
+
+    //입력된 정보에 해당하는 서버에 접속 시도 실패시 프로세스 종료
+    if(connect(clientSocket, (struct sockaddr*)&server_address, sizeof(server_address))==-1)
+    {
+        printf("sorry, connection failed! exit this process:%d\n",PID_NUM);
+        return 0;
+    }
+    else{ //연결에 성공했을 경우
+        for(int i = 0 ; i<5 ; i++){ // 메시지에 대해 5번 수행
+            char * toServer = malloc(sizeof(char)*100);
+            char temp[] = "Hello, Server this is message No." ;//서버에 보낼 문자열
+            char fromServer[100];
+            strcpy(toServer,temp); //임시변수에 들은 문자열 복사
+            strcat(toServer,(char*)i);//서버에 보낼 문자열에 sequence adding
+            strcat(toServer,"from"); //from adding
+            strcat(toServer,(char*)PID_NUM);//PID_NUM adding
+
+            /*toServer 문자열을 server로 쏩니다*/
+            printf("Client[PID:%d] now sending to Server\n",PID_NUM);
+            write(clientSocket,toServer,sizeof(toServer));
+            read(clientSocket, fromServer, sizeof(fromServer));
+            printf("Client[PID:%d] got message to Server%s\n",PID_NUM,fromServer);
+            free(toServer);  
+
+        }
+
+    }
+    sleep(4);   
+    }
+    }
+    printf("all process is terminated ");
 }
--- #2/server.c
+++ #2/server.c
@@ -5,18 +5,16 @@
 #include <string.h> 
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 5500
+#define PORT 10829
 
 int main(){
 
 int s_socket_fd; 
 int c_socket_fd;
+int fd_array[10][2]={{0,0}}; // [10]각 클라이언트 별로 생성되는 소켓 디스크립터 변수를 저장, [2]:메시지 시퀀스 확인용
 struct sockaddr_in server_address, client_address;
 socklen_t client_address_size;
-//char Msg2Client[]="Hello Client!!\n"; 
-//char Msg2Server[100];
-//char fromClient[100];
-//char toClient[100];
+
 
 /*클라이언트의 접속을 기다리는 서버소켓 객체 생성*/ 
 s_socket_fd = socket(PF_INET, SOCK_STREAM, 0);
@@ -32,55 +30,57 @@ server_address.sin_port=htons(PORT);
 bind(s_socket_fd, (struct sockaddr*) &server_address, sizeof(server_address));
 
 
-/*클라이언트의 접속을 기다리는 대기 모드로 전환*/ 
-listen(s_socket_fd, 5);
+/*클라이언트의 접속을 기다리는 대기 모드로 전환 - 본 문제에서는 10개의 백로그를 갖습니다.*/ 
+listen(s_socket_fd, 10);
 printf("Wait Client...\n");
 
+while(1){
 client_address_size = sizeof(client_address);
-c_socket_fd = accept(s_socket_fd, (struct sockaddr*)&client_address,&client_address_size);
-if(c_socket_fd < 0)
+c_socket_fd = accept(s_socket_fd, (struct sockaddr*)&client_address,&client_address_size);// 연결 수락
+
+if(c_socket_fd < 0) //소켓 디스크립터 오류시
 {
     printf("Server: accept failed.\n");
     exit(0);
 }
+else{ // 정상적으로 소켓 디스크립터가 들어올 경우
+    printf("where?");
+    int flag = 0;// fd_array를 스택 처럼 작동시키기 위해, 최 상위 가르킴
+    for(int i = 0; i<10 ; i++){//백로그 갯수만큼 공간 생성 및 순회 
 
-/*클라이언트가 서버에 접속하면 접속을 수락함*/ 
-printf("Client Connect!!!\n");
-
-while(1){
-    //
-/*클라이언트가 전달한 메시지를 읽어 들여서 fromClient에 대입*/ 
-char toClient[100];
-char fromClient[100];
-
-read(c_socket_fd, fromClient ,sizeof(fromClient));
-
-printf("From Client Message: %s\n",fromClient);
-if(strcmp("Bye",fromClient)==0)
-{   
-    printf("Connection is closed.\n");
-    break;
+        if(fd_array[i][0]==c_socket_fd)//이미 받았던 fd라면
+        {   
+            fd_array[i][1]++;
+            char fromClient[100]; // Client로 부터받은 메시지 저장 문자열 배열
+            char * toClient = malloc(sizeof(char)*100);// Client로 보낼 메시지 배열
+            char temp[] ="Hello, Client, this is my reply for your message No.";
+            strcpy(toClient,temp);
+            strcat(toClient,(char*)fd_array[i][1]);
+            read(c_socket_fd, fromClient ,sizeof(fromClient));
+            printf("From Client Message: %s\n",fromClient);
+            printf("Now sending Message To Client\n");
+            write(c_socket_fd, toClient, sizeof(toClient));
+            free(toClient);
+            break;
+        }
+        else if(i==9)//처음 받는 fd 일 경우 스택에 저장
+        {
+            fd_array[flag][0]=c_socket_fd;//flag에 해당하는 위치에 fd 저장
+            fd_array[flag][1]=0;
+            char fromClient[100]; // Client로 부터받은 메시지 저장 문자열 배열
+            char * toClient = malloc(sizeof(char)*100);// Client로 보낼 메시지 배열
+            char temp[] ="Hello, Client, this is my reply for your message No.";
+            strcpy(toClient,temp);
+            strcat(toClient,(char*)fd_array[i][1]);
+            read(c_socket_fd, fromClient ,sizeof(fromClient));
+            printf("From Client Message: %s\n",fromClient);
+            printf("Now sending Message To Client\n");
+            write(c_socket_fd, toClient, sizeof(toClient));
+            flag++;
+        }
+    }
+    }
 }
-printf("\nMessage To Client:");
-
-scanf("%[^\n]s",toClient);
-fflush(stdin);
-
-/* 이 자리에서 Client로부터 요청받은 서비스를 수행 */
-/* Msg2Client 의 내용을 클라이언트로 전송 */
-//if(toClient!=NULL){ 
-write(c_socket_fd, toClient, sizeof(toClient));
-//}
-
-//printf("Message To Client: %s\n",Msg2Client);
-/*클라이언트와의 접속을 종료함*/
-if(c_socket_fd < 0)
-{
-    printf("Server: connection closed.\n");
-    exit(0);
-}
-} 
-
 close(c_socket_fd);
 return 0;
 }
